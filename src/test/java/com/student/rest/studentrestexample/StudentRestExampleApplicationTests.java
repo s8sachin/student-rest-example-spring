20dc495204c80dee888cbe14ba818d2730521109
s8sachin <s.sachin94@gmail.com>
2020-09-15 17:53:28 +0530

    initial commit

---
+++ src/test/java/com/student/rest/studentrestexample/StudentRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.rest.studentrestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
